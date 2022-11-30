317f92cdfee60e5447571bf23573f2bddddecf70
Jr Bytes <itamazor@gmail.com>
2022-11-30 05:51:56 -0300

    added bannerslider component

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,45 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import BannerSlider, { BannerSliderProps } from '.'
+
+const items = [
+  {
+    img: '/images/1.jpg',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: '/images/2.jpg',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: '/images/3.jpg',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<BannerSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+)
