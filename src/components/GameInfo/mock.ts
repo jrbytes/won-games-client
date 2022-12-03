048fad41d1e0c008e1e3d1d68ba48f779badb785
Jr Bytes <itamazor@gmail.com>
2022-12-03 12:00:52 -0300

    added gameinfo components

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,5 @@
+export default {
+  title: 'Red Dead is back!',
+  description: 'Come see John’s new adventures',
+  price: '215,00'
+}
---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameInfo from '.'
+
+const props = {
+  title: 'Population Zero',
+  description: 'Game Description',
+  price: '60.00'
+}
+
+describe('<GameInfo />', () => {
+  it('should be able to render game informations', () => {
+    const { container } = renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: 'Population Zero' })
+    ).toBeInTheDocument()
+    expect(screen.getByText('Game Description')).toBeInTheDocument()
+    expect(screen.getByText('$ 60.00')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should be able to render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+  })
+})
