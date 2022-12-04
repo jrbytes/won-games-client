ebcc194b26218531d52788ac35b48e7c3785914f
Jr Bytes <itamazor@gmail.com>
2022-12-04 19:24:01 -0300

    added gallery components

---
+++ src/components/Gallery/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: '/images/games/cyberpunk-1.jpg',
+    label: 'Gallery Image 1'
+  },
+  {
+    src: '/images/games/cyberpunk-2.jpg',
+    label: 'Gallery Image 2'
+  },
+  {
+    src: '/images/games/cyberpunk-3.jpg',
+    label: 'Gallery Image 3'
+  },
+  {
+    src: '/images/games/cyberpunk-4.jpg',
+    label: 'Gallery Image 4'
+  },
+  {
+    src: '/images/games/cyberpunk-5.jpg',
+    label: 'Gallery Image 5'
+  },
+  {
+    src: '/images/games/cyberpunk-6.jpg',
+    label: 'Gallery Image 6'
+  }
+]
