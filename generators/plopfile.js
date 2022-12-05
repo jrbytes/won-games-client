5266fd79a2c8ae5450529b9322400abdd6980e44
Jr Bytes <itamazor@gmail.com>
2022-12-05 13:17:40 -0300

    updated how generate files with plop and updated plop

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,32 +1,104 @@
+const Component = {
+  FULL: 'Complete component with typing and function?',
+  SIMPLE: 'Simple component?'
+}
+
 module.exports = (plop) => {
-  plop.setGenerator('component', {
+  plop.setGenerator('Component', {
     description: 'Create a component',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'What is your component name?'
+        message: "What's your component name?"
+      },
+      {
+        type: 'list',
+        name: 'type',
+        message: 'What type of component is it?',
+        choices: [Component.FULL, Component.SIMPLE]
+      }
+    ],
+    actions: function (data) {
+      const actions = []
+
+      if (data.type === Component.FULL) {
+        actions.push(
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/index.tsx',
+            templateFile: 'templates/ComponentFull.tsx.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/styles.ts',
+            templateFile: 'templates/styles.ts.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/stories.tsx',
+            templateFile: 'templates/storiesFull.tsx.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/test.tsx',
+            templateFile: 'templates/test.tsx.hbs'
+          }
+        )
+      }
+
+      if (data.type === Component.SIMPLE) {
+        actions.push(
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/index.tsx',
+            templateFile: 'templates/Component.tsx.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/styles.ts',
+            templateFile: 'templates/styles.ts.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/stories.tsx',
+            templateFile: 'templates/stories.tsx.hbs'
+          },
+          {
+            type: 'add',
+            path: '../src/components/{{pascalCase name}}/test.tsx',
+            templateFile: 'templates/test.tsx.hbs'
+          }
+        )
+      }
+
+      return actions
+    }
+  })
+
+  plop.setGenerator('Template', {
+    description: 'Create a template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: "What's your template name?"
       }
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/index.tsx',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/Component.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/styles.ts',
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
+        path: '../src/templates/{{pascalCase name}}/test.tsx',
         templateFile: 'templates/test.tsx.hbs'
       }
     ]
