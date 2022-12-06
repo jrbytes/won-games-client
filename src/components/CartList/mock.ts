6b69d074bc5198da574173b39c5ba8f3e9497fac
Jr Bytes <itamazor@gmail.com>
2022-12-06 06:26:21 -0300

    added cartlist components

---
+++ src/components/CartList/mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 235,00'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Bordelands 3',
+    price: 'R$ 235,00'
+  }
+]
