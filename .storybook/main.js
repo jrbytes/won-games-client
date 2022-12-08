2eab63b021cc12a9e7529de868a01b226156831b
Jr Bytes <itamazor@gmail.com>
2022-12-08 08:19:02 -0300

    updated app to use npm management

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,15 @@ module.exports = {
   addons: ['@storybook/addon-essentials'],
   babel: async (options) => ({
     ...options,
-    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+    plugins: [
+      ...options.plugins,
+      '@babel/plugin-transform-react-jsx',
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+    ]
   }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
-  }
+  },
+  typescript: { reactDocgen: 'react-docgen' }
 }
