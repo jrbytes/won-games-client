3727bbf6aa839e28685a7ac185a24cdcd9f9c45a
Jr Bytes <itamazor@gmail.com>
2022-12-08 10:16:23 -0300

    added games templates and games page

---
+++ src/pages/games.tsx
@@ -0,0 +1,17 @@
+import Games, { GamesProps } from 'templates/Games'
+
+import filterItemsMock from 'components/ExploreSidebar/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+
+export default function GamesPage(props: GamesProps) {
+  return <Games {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      games: gamesMock,
+      filterItems: filterItemsMock
+    }
+  }
+}
---
+++ src/templates/Games/index.tsx
@@ -0,0 +1,46 @@
+import Base from 'templates/Base'
+import { Grid } from 'components/Grid'
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
+import GameCard, { GameCardProps } from 'components/GameCard'
+
+import * as S from './styles'
+import { KeyboardArrowDown } from '@styled-icons/material-outlined'
+
+export type GamesProps = {
+  games?: GameCardProps[]
+  filterItems: ItemProps[]
+}
+
+const Games = ({ games, filterItems }: GamesProps) => {
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+
+        <section>
+          <Grid>
+            {games &&
+              games.map((game, index) => (
+                <GameCard key={`game-card-${index}`} {...game} />
+              ))}
+          </Grid>
+
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show More</p>
+            <KeyboardArrowDown size={35} />
+          </S.ShowMore>
+        </section>
+      </S.Main>
+    </Base>
+  )
+}
+
+export default Games
