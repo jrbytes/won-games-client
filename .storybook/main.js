4fe81030adc5aec9697800882f0d8e5cd0570e4c
Jr Bytes <itamazor@gmail.com>
2022-12-10 09:20:54 -0300

    updated npm management to node 18, updated mdx to version 2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,29 @@
-const path = require("path")
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
 
 module.exports = {
-  "stories": ['../src/components/**/stories.tsx'],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+  stories: ['../src/components/**/stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
-      name: "storybook-addon-next",
+      name: 'storybook-addon-next',
       options: {
-        nextConfigPath: path.resolve(__dirname, "../next.config.js"),
-      },
+        nextConfigPath: path.resolve(__dirname, '../next.config.js')
+      }
     }
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
   },
   docs: {
-    docsPage: "automatic"
+    docsPage: 'automatic'
   },
   typescript: { reactDocgen: 'react-docgen' },
-  staticDirs: ['../public']
-};
+  staticDirs: ['../public'],
+  features: {
+    previewMdx2: true
+  }
+}
